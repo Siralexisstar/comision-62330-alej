5247d0587c9ae3690f3920e1868b070a8eb4a0b3
SiralexisStar <alexgarciavillar96@gmail.com>
2024-07-07 20:39:26 +0200

    Adding modules to css styles

--- src/components/ItemListContainer/ItemListCointainer.jsx
+++ src/components/ItemListContainer/ItemListCointainer.jsx
@@ -1,12 +1,15 @@
 import ItemCount from "../ItemCount/ItemCount";
 import ItemList from "../ItemList/ItemList";
+import  styles  from "./ItemListCointainer.module.css";
 // Using this class as a container
 const ItemListContainer = ({ title }) => {
   return (
     <>
-      <h2>{title}</h2>
-      <ItemList />
-      <ItemCount stock={10} initial={1}/>
+      <div className= {styles.container}>
+        <h2 className={styles.title}>{title}</h2>
+        <ItemList />
+        <ItemCount stock={10} initial={1} />
+      </div>
     </>
   );
 };
