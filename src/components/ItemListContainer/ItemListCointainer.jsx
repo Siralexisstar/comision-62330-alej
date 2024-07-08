6f8072ae5bba66e3cb589fbc08b60027a9c41a53
SiralexisStar <alexgarciavillar96@gmail.com>
2024-07-08 09:20:03 +0200

    adding some styles

--- src/components/ItemListContainer/ItemListCointainer.jsx
+++ src/components/ItemListContainer/ItemListCointainer.jsx
@@ -4,13 +4,11 @@ import  styles  from "./ItemListCointainer.module.css";
 // Using this class as a container
 const ItemListContainer = ({ title }) => {
   return (
-    <>
-      <div className= {styles.container}>
-        <h2 className={styles.title}>{title}</h2>
-        <ItemList />
-        <ItemCount stock={10} initial={1} />
-      </div>
-    </>
+    <div className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <ItemList />
+      <ItemCount stock={10} initial={1} />
+    </div>
   );
 };
 
