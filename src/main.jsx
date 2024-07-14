a7cbe83d626dc178afda1cb5da8fb166edca681b
SiralexisStar <alexgarciavillar96@gmail.com>
2024-07-14 18:25:46 +0200

    fixing issues

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' //UpperCamelCase for components
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const rootElement = document.getElementById('root')
 
