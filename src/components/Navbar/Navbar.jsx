df720d4edee8996769560d3bcf271759b7b24476
SiralexisStar <alexgarciavillar96@gmail.com>
2024-07-14 18:03:03 +0200

    adding social media links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,16 +4,24 @@ import CartWidget from "../CartWidget/CartWidget";
 import "./Navbar.css";
 import LogoImg from "../Logo/LogoImg";
 import Search from "../Search/Search";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Navbar = ({ categorias, title }) => {
   console.log(categorias);
   return (
     <>
       <header className="header">
-        <div className="top-bar">
+      <div className="top-bar">
           <div className="social-icons">
-            <i className="fab fa-instagram"></i>
-            <i className="fab fa-facebook-f"></i>
+            <a href="https://www.instagram.com/velassere/?igsh=MTI5aG83b3gzdnB2YQ%3D%3D" target="_blank" rel="noopener noreferrer" className="social-icon social-icon-instagram">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon social-icon-facebook">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="social-icon social-icon-youtube">
+              <i className="fab fa-youtube"></i>
+            </a>
           </div>
           <div className="logo-container">
             <LogoImg />
