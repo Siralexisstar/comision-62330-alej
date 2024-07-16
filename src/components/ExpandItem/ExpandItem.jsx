4a12da963787715d88130add21f9e3f2c3878e67
SiralexisStar <alexgarciavillar96@gmail.com>
2024-07-16 17:44:31 +0200

    añadiendo itemList para la tienda

--- src/components/ExpandItem/ExpandItem.jsx
+++ src/components/ExpandItem/ExpandItem.jsx
@@ -1,9 +1,9 @@
 export const ExpandItem = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNavDarkDropdown"
@@ -11,37 +11,47 @@ export const ExpandItem = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown">
+        <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                INICIO
+              </a>
+            </li>
+            <li className="nav-item dropdown">
               <a
-                class="nav-link dropdown-toggle"
+                className="nav-link dropdown-toggle"
                 href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Dropdown
+                TIENDA
               </a>
-              <ul class="dropdown-menu dropdown-menu-dark">
+              <ul className="dropdown-menu dropdown-menu-dark">
                 <li>
-                  <a class="dropdown-item" href="#">
-                    Action
+                  <a className="dropdown-item" href="#">
+                    VELAS AROMATICAS
                   </a>
                 </li>
                 <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
+                  <a className="dropdown-item" href="#">
+                    VELAS SIN AROMA
                   </a>
                 </li>
               </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                QUIENES SOMOS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                CONTACTO
+              </a>
             </li>
           </ul>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import CartWidget from "../CartWidget/CartWidget";
 import "./Navbar.css";
 import LogoImg from "../Logo/LogoImg";
 import Search from "../Search/Search";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import ExpandItem from "../ExpandItem/ExpandItem";
 
 const Navbar = ({ categorias, title }) => {
@@ -12,15 +12,30 @@ const Navbar = ({ categorias, title }) => {
   return (
     <>
       <header className="header">
-      <div className="top-bar">
+        <div className="top-bar">
           <div className="social-icons">
-            <a href="https://www.instagram.com/velassere/?igsh=MTI5aG83b3gzdnB2YQ%3D%3D" target="_blank" rel="noopener noreferrer" className="social-icon social-icon-instagram">
+            <a
+              href="https://www.instagram.com/velassere/?igsh=MTI5aG83b3gzdnB2YQ%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon social-icon-instagram"
+            >
               <i className="fab fa-instagram"></i>
             </a>
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon social-icon-facebook">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon social-icon-facebook"
+            >
               <i className="fab fa-facebook-f"></i>
             </a>
-            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="social-icon social-icon-youtube">
+            <a
+              href="https://www.youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon social-icon-youtube"
+            >
               <i className="fab fa-youtube"></i>
             </a>
           </div>
@@ -33,23 +48,7 @@ const Navbar = ({ categorias, title }) => {
             <span>ES</span>
           </div>
         </div>
-        <div className="navbar-container">
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav mx-auto">
-                  {menu.map((menu, index) => (
-                    <li className="nav-item" key={index}>
-                      <a className="nav-link" href="#">
-                        {menu}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </div>
+        <ExpandItem />
       </header>
     </>
   );
